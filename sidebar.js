07be146f862232fc6357a88f030fa5ba823f6841
Ahmed Nasr <ahmedhosna95@gmail.com>
2019-04-30 07:41:56 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -16,11 +16,3 @@ jQuery(document)['ready'](function() {
     }
 
 );
-
-$(document)['ready'](function() {
-        $('#dontedit')['html'](' - \u062A\u0639\u0631\u064A\u0628 : <a href="https://www.tharabic.com" rel="dofollow" target="_blank" style="visibility:visible!important;opacity:1!important;position:relative!important">\u0627\u0644\u0645\u062D\u062A\u0631\u0641 \u0627\u0644\u0639\u0631\u0628\u064A</a>'), setInterval(function() {
-                $('#dontedit:visible')['length'] || (window['location']['href'] = 'https://www.tharabic.com')
-            }
-            , 3e3)
-    }
-)
